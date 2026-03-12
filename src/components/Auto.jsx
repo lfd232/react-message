fab7be6281423eca9099894a4862e700c0f3ecaa
unknown <egorak2000@gmail.com>
2026-03-12 11:21:50 +0300

    'Calendar'

--- src/components/Auto.jsx
+++ src/components/Auto.jsx
@@ -1,28 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
  const Auto = () => {
+  const [date, setDate]=useState("2026-03-12")
+  const handleClick=(event)=>{
+    setDate(event.target.value)
+    console.log(date)
+  }
   return (
     <div className='form'>
-        <input
+        <p>REGISTER</p>
+
+                <input
+        className="in3"
+        type = "text"
+        id = "register"
+        name ="register"
+        required = "8"
+        maxlenght = "15"
+        size = "30"
+        placeholder='Username'  />   
         
+        <input
+        className="in1"
         type = "text"
         id = "name"
         name ="name"
         required = "4"
         maxlenght = "8"
-        size = "30"  />    
+        size = "30"
+        placeholder='Email'  />    
+
         <input
+        className="in2"
         type = "text"
         id = "password"
         name ="password"
         required = "8"
         maxlenght = "15"
-        size = "30"  />   
-        
-        
+        size = "30"
+        placeholder='Password'  />
 
+        <input
+        onChange={handleClick}
+    type="date"
+    id="start"
+    value={date}
+    name="trip-start"
+    min="1900-01-01"
+    max="2026-12-03" />
+
+  
+
+
+        <button className="submit">Sign up</button>
+        
     </div>
   )
 }
