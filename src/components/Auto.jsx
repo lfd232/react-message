55495bc6a542605f006997e6c201f7977a974ae7
lfd232 <smzylfg@MacBook-Pro-smzylfg.local>
2026-03-12 10:12:03 -0700

    app

--- src/components/Auto.jsx
+++ src/components/Auto.jsx
@@ -6,19 +6,20 @@ import React from 'react'
   return (
     <div className='form'>
         <input
+        
         type = "text"
         id = "name"
         name ="name"
         required = "4"
         maxlenght = "8"
-        size = "10"  />    
+        size = "30"  />    
         <input
         type = "text"
         id = "password"
         name ="password"
         required = "8"
         maxlenght = "15"
-        size = "10"  />   
+        size = "30"  />   
         
         
 
