0c91d9e1bec2c61f7ebe54ad418d952ff03c0a9d
stockd1 <egorak2000@gmail.com>
2026-03-26 15:24:23 +0300

    upd

--- src/components/workspace.jsx
+++ src/components/workspace.jsx
@@ -5,12 +5,24 @@ export const Workspace = () => {
     <div className={styles.workspace}>
       <div className={styles.highbar}>
         <div className={styles.arrow}></div>
+
         <div className={styles.companionmain}>
         <div className={styles.companion}></div>
-        <div className={styles.companionname}>Firstname Lastname</div>
+        <div className={styles.companionname}>Firstname Lastname
+          <div className={styles.companionupdate}>Has been updated 1 day ago</div>
         </div>
+        </div>
+      
+      
+
       </div>
       
+    <div className={styles.messagearea}></div>
+
+    <div className={styles.entryarea}>
+      <div className={styles.send}></div>
+    </div>
+
     </div>
 
   )
