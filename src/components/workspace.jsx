b1f302e4bdab8eb1bbc88a400439ae3754ea91e1
unknown <egorak2000@gmail.com>
2026-03-26 11:18:24 +0300

    workspaceupd

--- src/components/workspace.jsx
+++ src/components/workspace.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import styles from './workspace.module.css'
 export const Workspace = () => {
   return (
-    <div className={styles.workspace}></div>
+    <div className={styles.workspace}>
+      <div className={styles.highbar}>
+        <div className={styles.arrow}></div>
+        <div className={styles.companionmain}>
+        <div className={styles.companion}></div>
+        <div className={styles.companionname}>Firstname Lastname</div>
+        </div>
+      </div>
+      
+    </div>
+
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import styles from "./home.module.css"
+import {Workspace} from "../components/workspace"
 export const Home = () => {
   return (
     
     <div className={styles.page}> 
         <Sidebar/>
-        
+        <Workspace/>
     </div>
     
     
